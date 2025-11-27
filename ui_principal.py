400aefa088673494b462811358c49584afd7d3de
colo <j.scarlato.garcia@itsv.edu.ar>
2025-11-27 08:50:01 -0300

    Faltan colores

--- ui_principal.py
+++ ui_principal.py
@@ -1,7 +1,7 @@
 # -*- coding: utf-8 -*-
 
 ################################################################################
-## Form generated from reading UI file 'main_windowOaaqGR.ui'
+## Form generated from reading UI file 'main_windowgJiSkg.ui'
 ##
 ## Created by: Qt User Interface Compiler version 6.9.3
 ##
@@ -15,38 +15,80 @@ from PySide6.QtGui import (QBrush, QColor, QConicalGradient, QCursor,
     QFont, QFontDatabase, QGradient, QIcon,
     QImage, QKeySequence, QLinearGradient, QPainter,
     QPalette, QPixmap, QRadialGradient, QTransform)
-from PySide6.QtWidgets import (QApplication, QMainWindow, QMenuBar, QPushButton,
-    QSizePolicy, QStatusBar, QWidget)
+from PySide6.QtWidgets import (QApplication, QGridLayout, QLabel, QMainWindow,
+    QMenuBar, QPushButton, QSizePolicy, QSpacerItem,
+    QStatusBar, QWidget)
 
 class Ui_MainWindow(object):
     def setupUi(self, MainWindow):
         if not MainWindow.objectName():
             MainWindow.setObjectName(u"MainWindow")
-        MainWindow.resize(712, 600)
+        MainWindow.resize(1099, 533)
         self.centralwidget = QWidget(MainWindow)
         self.centralwidget.setObjectName(u"centralwidget")
-        self.pushButton_3 = QPushButton(self.centralwidget)
-        self.pushButton_3.setObjectName(u"pushButton_3")
-        self.pushButton_3.setGeometry(QRect(280, 250, 151, 41))
-        self.pushButton_4 = QPushButton(self.centralwidget)
-        self.pushButton_4.setObjectName(u"pushButton_4")
-        self.pushButton_4.setGeometry(QRect(280, 190, 151, 41))
-        self.pushButton_5 = QPushButton(self.centralwidget)
-        self.pushButton_5.setObjectName(u"pushButton_5")
-        self.pushButton_5.setGeometry(QRect(280, 310, 151, 41))
-        self.pushButton_6 = QPushButton(self.centralwidget)
-        self.pushButton_6.setObjectName(u"pushButton_6")
-        self.pushButton_6.setGeometry(QRect(280, 130, 151, 41))
+        self.gridLayout = QGridLayout(self.centralwidget)
+        self.gridLayout.setObjectName(u"gridLayout")
+        self.verticalSpacer = QSpacerItem(248, 538, QSizePolicy.Policy.Minimum, QSizePolicy.Policy.Expanding)
+
+        self.gridLayout.addItem(self.verticalSpacer, 1, 0, 6, 1)
+
         self.pushButton_7 = QPushButton(self.centralwidget)
         self.pushButton_7.setObjectName(u"pushButton_7")
-        self.pushButton_7.setGeometry(QRect(280, 70, 151, 41))
+        self.pushButton_7.setMaximumSize(QSize(16777215, 45))
+
+        self.gridLayout.addWidget(self.pushButton_7, 2, 1, 1, 1)
+
+        self.pushButton_3 = QPushButton(self.centralwidget)
+        self.pushButton_3.setObjectName(u"pushButton_3")
+        self.pushButton_3.setMaximumSize(QSize(16777215, 45))
+
+        self.gridLayout.addWidget(self.pushButton_3, 5, 1, 1, 1)
+
+        self.pushButton_6 = QPushButton(self.centralwidget)
+        self.pushButton_6.setObjectName(u"pushButton_6")
+        self.pushButton_6.setMaximumSize(QSize(16777215, 45))
+
+        self.gridLayout.addWidget(self.pushButton_6, 3, 1, 1, 1)
+
+        self.pushButton_4 = QPushButton(self.centralwidget)
+        self.pushButton_4.setObjectName(u"pushButton_4")
+        self.pushButton_4.setMaximumSize(QSize(16777215, 45))
+
+        self.gridLayout.addWidget(self.pushButton_4, 4, 1, 1, 1)
+
+        self.pushButton_5 = QPushButton(self.centralwidget)
+        self.pushButton_5.setObjectName(u"pushButton_5")
+        self.pushButton_5.setMaximumSize(QSize(16777215, 45))
+
+        self.gridLayout.addWidget(self.pushButton_5, 6, 1, 1, 1)
+
+        self.verticalSpacer_2 = QSpacerItem(288, 538, QSizePolicy.Policy.Minimum, QSizePolicy.Policy.Expanding)
+
+        self.gridLayout.addItem(self.verticalSpacer_2, 1, 2, 6, 1)
+
         self.pushButton_8 = QPushButton(self.centralwidget)
         self.pushButton_8.setObjectName(u"pushButton_8")
-        self.pushButton_8.setGeometry(QRect(280, 10, 151, 41))
+        self.pushButton_8.setMaximumSize(QSize(16777215, 45))
+
+        self.gridLayout.addWidget(self.pushButton_8, 1, 1, 1, 1)
+
+        self.label = QLabel(self.centralwidget)
+        self.label.setObjectName(u"label")
+        self.label.setMaximumSize(QSize(250, 60))
+        font = QFont()
+        font.setPointSize(20)
+        self.label.setFont(font)
+        self.label.setLayoutDirection(Qt.LayoutDirection.LeftToRight)
+        self.label.setLineWidth(5)
+        self.label.setTextFormat(Qt.TextFormat.MarkdownText)
+        self.label.setAlignment(Qt.AlignmentFlag.AlignCenter)
+
+        self.gridLayout.addWidget(self.label, 0, 1, 1, 1)
+
         MainWindow.setCentralWidget(self.centralwidget)
         self.menubar = QMenuBar(MainWindow)
         self.menubar.setObjectName(u"menubar")
-        self.menubar.setGeometry(QRect(0, 0, 712, 33))
+        self.menubar.setGeometry(QRect(0, 0, 1099, 33))
         MainWindow.setMenuBar(self.menubar)
         self.statusbar = QStatusBar(MainWindow)
         self.statusbar.setObjectName(u"statusbar")
@@ -59,11 +101,12 @@ class Ui_MainWindow(object):
 
     def retranslateUi(self, MainWindow):
         MainWindow.setWindowTitle(QCoreApplication.translate("MainWindow", u"MainWindow", None))
+        self.pushButton_7.setText(QCoreApplication.translate("MainWindow", u"Registrarse", None))
         self.pushButton_3.setText(QCoreApplication.translate("MainWindow", u"Agendar Tareas/Examenes", None))
+        self.pushButton_6.setText(QCoreApplication.translate("MainWindow", u"Mostrar Horario", None))
         self.pushButton_4.setText(QCoreApplication.translate("MainWindow", u"Editar Horario", None))
         self.pushButton_5.setText(QCoreApplication.translate("MainWindow", u"Agendar Libros", None))
-        self.pushButton_6.setText(QCoreApplication.translate("MainWindow", u"Mostrar Horario", None))
-        self.pushButton_7.setText(QCoreApplication.translate("MainWindow", u"Registrarse", None))
         self.pushButton_8.setText(QCoreApplication.translate("MainWindow", u"Iniciar Sesion", None))
+        self.label.setText(QCoreApplication.translate("MainWindow", u"Agenda Digital", None))
     # retranslateUi
 
